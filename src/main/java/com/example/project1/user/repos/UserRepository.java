daeeea1ae850a8f6558c26d44bacf05a1db14441
aigerim111 <74233562+aigerim111@users.noreply.github.com>
2021-05-24 15:03:45 +0600

    Update UserRepository.java

--- src/main/java/com/example/project1/user/repos/UserRepository.java
+++ src/main/java/com/example/project1/user/repos/UserRepository.java
@@ -2,10 +2,10 @@ package com.example.project1.user.repos;
 
 import com.example.project1.user.Usr;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
